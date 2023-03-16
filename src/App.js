3eb9055b6f7ef29bcf94b8c6bd28283ca36d9221
Daniel <daniel.nastase@gmail.com>
2023-03-16 12:43:58 +0200

    react router install

--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@ import './App.css';
 import yummyData from './data/yummy.json';
 
 
+
+const FoodItem = ()=> {
+
+}
+
+
+const FoodCategory = ({items})=> {
+  return (<>
+    {Object.values(items).map(item => {
+      return (<div className='item'>
+        <h5>{item.name} - {item.price}</h5>
+      </div>)
+    })}
+  </>)
+}
+
+const FoodMenu = ({data})=> {
+  return (<>
+    {Object.values(data).map(category => {
+      return (<div className='category'>
+        <h2>{category.name}</h2>
+        <FoodCategory items={category.items} />
+      </div>)
+    })}
+  </>)
+}
+
 function App() {
   console.log(yummyData)
   return (
-    <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
-    </div>
+    <FoodMenu data={yummyData.foods} />
   );
 }
 
