de80d5cc1751e30d4b153b4e6571f1a101a49d39
Daniel <daniel.nastase@gmail.com>
2023-03-16 11:23:13 +0200

    create react app stuff

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import yummyData from './data/yummy.json';
+
+
+function App() {
+  console.log(yummyData)
+  return (
+    <div className="App">
+      <header className="App-header">
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
